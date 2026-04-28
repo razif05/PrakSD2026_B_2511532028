30a1fdb2f58dc8496f6569bb28476ddea1b2ea4a
razif05 <razif050907@gmail.com>
2026-04-28 09:53:35 +0700

    Praktikum Pekan 4

--- src/pekan4_2511532028/QueueArrayDriver_2511532028.java
+++ src/pekan4_2511532028/QueueArrayDriver_2511532028.java
@@ -3,21 +3,21 @@ package pekan4_2511532028;
 public class QueueArrayDriver_2511532028 {
 
 	public static void main(String[] args) {
-		QueueArray_2511532028 queue = new QueueArray_2511532028(1000);
-		queue.enqueue_2028(10);
-		queue.enqueue_2028(20);
-		queue.enqueue_2028(30);
-		queue.enqueue_2028(40);
-		System.out.println("item di depan " + queue.front_2028);
-		System.out.println("item paling belakang " + queue.rear_2028);
+		QueueArray_2511532028 queue_2028 = new QueueArray_2511532028(1000);
+		queue_2028.enqueue_2028(10);
+		queue_2028.enqueue_2028(20);
+		queue_2028.enqueue_2028(30);
+		queue_2028.enqueue_2028(40);
+		System.out.println("item di depan " + queue_2028.front_2028);
+		System.out.println("item paling belakang " + queue_2028.rear_2028);
 		System.out.println("tampilan queue");
-		queue.display_2028();
+		queue_2028.display_2028();
 		System.out.println();
-		System.out.println(queue.dequeue_2028() + " dihapus dari queue ");
-		System.out.println("item di depan : " + queue.front_2028);
-		System.out.println("item paling belakang : " + queue.rear_2028);
+		System.out.println(queue_2028.dequeue_2028() + " dihapus dari queue ");
+		System.out.println("item di depan : " + queue_2028.front_2028);
+		System.out.println("item paling belakang : " + queue_2028.rear_2028);
 		System.out.println("tampilan queue setelah satu data dihapus");
-		queue.display_2028();
+		queue_2028.display_2028();
 
 	}
 
